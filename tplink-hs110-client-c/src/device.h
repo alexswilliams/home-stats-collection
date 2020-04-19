afe929a1e8085c87bf5ecea045129d56d5d45359
Alex Williams <alex.williams@skybettingandgaming.com>
2020-04-19 03:39:44 +0100

    Use external image for tplink scraper

--- tplink-hs110-client-c/src/device.h
+++
@@ -1,8 +0,0 @@
-#ifndef TPLINK_HS110_METRICS_CLIENT_DEVICE_H
-#define TPLINK_HS110_METRICS_CLIENT_DEVICE_H
-
-#include <cjson/cJSON.h>
-
-int query_device(int connection, const char *request, cJSON **out);
-
-#endif //TPLINK_HS110_METRICS_CLIENT_DEVICE_H
